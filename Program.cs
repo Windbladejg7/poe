7300fe24412e468e78dd2dd6968c0d8bfae9d89e
Windbladejg7 <juliangarzon867@gmail.com>
2025-11-09 17:45:40 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Controlador;
+
+namespace Vista
+{
+   internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsolaHelper consola = new ConsolaHelper();
+            Console.WriteLine("Datos del Juego");
+            int id = consola.LeerNumero("Ingrese el id: ");
+            string nombre = consola.LeerString("Ingrese un nombre para el juego: ");
+            string descripcion = consola.LeerString("Ingrese una descripcion para el juego: ");
+
+            JuegoControlador juegoControlador = new JuegoControlador();
+            juegoControlador.AgregarJuego(id, nombre, descripcion);
+        }
+    }
+}
